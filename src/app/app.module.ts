92e3078451467a059bdc3aadf4b5a4e245e7bfc4
ravindra.gaikawad <ravindra.gaikwad@mobiliya.com>
2019-02-19 10:58:17 +0530

    intially required componenets added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    AuthorsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
