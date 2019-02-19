97b4aa2e1a040814c636303876c616a24817d75d
ravindra.gaikawad <ravindra.gaikwad@mobiliya.com>
2019-02-19 13:20:47 +0530

    authors/:id/quotes route enabled

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Quote } from '../models/quote';
+import { AuthorService } from '../author.service';
 
 @Component({
   selector: 'app-authors',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authors.component.css']
 })
 export class AuthorsComponent implements OnInit {
+  quotes: Quote[];
+  constructor(
+    private route: ActivatedRoute,
+    private authorService: AuthorService) { }
 
-  constructor() { }
-
+  
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.getQuotes(id);
   }
 
+  getQuotes(authorId:number): void {
+    this.authorService.getQuotes(authorId)
+    .subscribe(quotes => this.quotes = quotes);
+  }
+
+
 }
+
